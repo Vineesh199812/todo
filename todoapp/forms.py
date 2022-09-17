4daa4d6eff654c66372af3c18a0288d39371cc0a
Vineesh <vineeshm199812@gmail.com>
2022-09-17 11:33:48 +0530

    todoapp17-09-2022

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from todoapp.models import Todo
 
 # class RegistrationForm(forms.Form):
 #     first_name=forms.CharField()
@@ -23,3 +24,12 @@ class RegistrationForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
     password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model=Todo
+        fields=["task_name"]
+
+        widgets={
+            "task_name":forms.TextInput(attrs={"class":"form-control"})
+        }
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from todoapp import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from todoapp.models import Todo
 
 # Create your views here.
 
@@ -50,3 +51,33 @@ class SignOutView(View):
     def get(self,request,*args,**kwargs):
         logout(request)
         return redirect("signin")
+
+class TodoAddView(View):
+    def get(self,request,*args,**kwargs):
+        form=forms.TodoForm()
+        return render(request,"add-todo.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=forms.TodoForm(request.POST)    #works only if view is modelview  otherwise >>
+        if form.is_valid():                  #Todo.object.create(**form.cleaned_data,user=request.user)  #import todo from Todo
+            form.instance.user=request.user
+            form.save()
+            return redirect("index")
+        else:
+            return render(request,"add-todo.html",{"form":form})
+
+class TodoListView(View):
+    def get(self,request,*args,**kwargs):
+        all_todos=Todo.objects.filter(user=request.user)
+        return render(request,"todolist.html",{"todos":all_todos})
+
+# localhost:8000/todos/remove/<int:id>
+def delete_todo(request,*args,**kwargs):
+    id=kwargs.get("id")
+    Todo.objects.get(id=id).delete()
+    return redirect("todos-list")
+
+class TodoDetailView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("id")
+        todo=Todo.objects.get(id=id)
+        return render(request,"todo-detail.html",{"todo":todo})
